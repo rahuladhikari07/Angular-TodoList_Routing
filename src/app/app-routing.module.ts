f793b7b6062881bf4adc2618d3462e97a9af01b6
rahuladhikari07 <rahul.adhikari2018@gmail.com>
2022-09-26 08:44:31 -0700

    adding files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './MyComponenets/about/about.component';
+import { HomeComponent } from './MyComponenets/home/home.component';
+import { TodosComponent } from './MyComponenets/todos/todos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TodosComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'home', component: HomeComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './MyComponenets/todos/todos.component';
+import { TodoItemComponent } from './MyComponenets/todo-item/todo-item.component';
+import { AddTodosComponent } from './MyComponenets/add-todos/add-todos.component';
+import {FormsModule} from '@angular/forms';
+import { AboutComponent } from './MyComponenets/about/about.component';
+import { HomeComponent } from './MyComponenets/home/home.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    AddTodosComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
